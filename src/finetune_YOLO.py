27f99b51f30831e2979a840a761e9c2f89ee013e
SeminOH <dhtpals@snu.ac.kr>
2025-05-10 20:36:16 +0900

    'finetune_YOLO

---
+++ src/finetune_YOLO.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+from data.prepare_dataset import prepare_dataset
+from ultralytics import YOLO
+from data.prepare_dataset import prepare_dataset
+from pathlib import Path
+import os
+
+
+os.chdir(Path(__file__).resolve().parents[1])
+
+model = YOLO("yolov8n.pt")
+
+yaml_path = prepare_dataset(
+    zip_path="data/datasets.zip",
+    extract_to="datasets/my_dataset"
+)
+
+model.train(
+    data=str(yaml_path),
+    epochs=100,
+    save_period=10,
+    project="runs/train",
+    name="finetune_exp1"
+)
