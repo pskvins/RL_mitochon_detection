eb65a22870b478009b7d03fed0bca2c41d249910
Seungyeon <102790547+seunnyeon@users.noreply.github.com>
2025-05-16 16:39:40 +0900

    Delete yolov8.py

--- yolov8.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
-
-# Train the model
-results = model.train(data="./yolov11_520/data.yaml", epochs=100, imgsz=416)
