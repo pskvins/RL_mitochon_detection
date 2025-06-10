Bug fixed: gt_boxes were updated in the dataloader, making overflow

src/data/coarse_boxes_loader.py
@@ -23,6 +23,7 @@ class CoarseBoxesDataset(Dataset):
             os.path.join(label_dir, os.path.splitext(os.path.basename(p))[0] + ".txt")
             for p in self.image_paths
         ]
+        self.labels = [self._load_groundtruth(label_path) for label_path in self.label_paths]
         self.coarse_paths = [
             os.path.join(coarse_box_dir, os.path.splitext(os.path.basename(p))[0] + ".npy")
             for p in self.image_paths
@@ -33,7 +34,12 @@ class CoarseBoxesDataset(Dataset):
 
     def __getitem__(self, idx):
         img = Image.open(self.image_paths[idx]).convert("RGB")
-        gt_boxes = self._load_groundtruth(self.label_paths[idx])  # [N, 4]
+        # gt_boxes = self._load_groundtruth(self.label_paths[idx])  # [N, 4]
+        gt_boxes = self.labels[idx]
+        # Check if gt_boxes is inf
+        if np.isinf(gt_boxes).any():
+            print("infinite from here!")
+            quit()
         coarse_boxes = np.load(self.coarse_paths[idx])            # [M, 5]
 
         return img, gt_boxes, coarse_boxes
@@ -43,8 +49,10 @@ class CoarseBoxesDataset(Dataset):
         with open(label_path, "r") as f:
             for line in f:
                 tokens = line.strip().split()
-                if len(tokens) < 5:
+                if len(tokens) != 5:
+                    print(f"Skipping {label_path} because it has {len(tokens)} tokens")
                     continue
                 _, x, y, w, h = map(float, tokens)
                 boxes.append([x, y, w, h])
-        return np.array(boxes, dtype=np.float32)
+        return boxes
+        # return np.array(boxes, dtype=np.float32)

src/env/utils.py
@@ -8,7 +8,7 @@ from torchvision.models import mobilenet_v2
 from ultralytics import YOLO
 import torch.nn.functional as F
 
-def compute_iou(box1: np.ndarray, box2: np.ndarray) -> float:
+def compute_iou(box1: np.ndarray, box2: np.ndarray, return_area: bool = False) -> float:
     """
     Compute IoU between two boxes in [x, y, w, h] format.
     """
@@ -35,12 +35,18 @@ def compute_iou(box1: np.ndarray, box2: np.ndarray) -> float:
         union_area = area1 + area2 - inter_area
 
         iou = inter_area / union_area
-        if not np.isfinite(iou):
+        if not np.isfinite(iou) or np.isnan(iou) or np.isinf(iou):
+            if return_area:
+                return 0.0, 0.0, area1, area2
             return 0.0
+        if return_area:
+            return iou, inter_area, area1, area2
         return iou
     
     except Exception as e:
         print(f"[compute_iou] Exception: {e}")
+        if return_area:
+            return 0.0, 0.0, 0.0, 0.0
         return 0.0
 
 

src/train_ddpg.py
@@ -63,7 +63,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 save_cfg = cfg["save"]
 save_interval = save_cfg['save_interval']
 checkpoint_dir = save_cfg["checkpoint_dir"]
-save_best_only = save_cfg.get("save_best_only", False)
+save_best_only = save_cfg.get("save_best_only", True)
 
 #save logs
 log_cfg = cfg["log"]
@@ -120,16 +120,19 @@ for epoch in range(epochs):
     for img, gt_boxes, coarse_boxes in tqdm(dataloader):
         for box in coarse_boxes:
             img_w, img_h = img.size
-            gt_boxes = np.array([
-                        [x * img_w, y * img_h, w * img_w, h * img_h]
-                        for x, y, w, h in gt_boxes], dtype=np.float32) #rescale
+            image_array = np.array([img_w, img_h, img_w, img_h], dtype=np.float32)
+            # gt_boxes = np.array([
+            #             [x * img_w, y * img_h, w * img_w, h * img_h]
+            #             for x, y, w, h in gt_boxes], dtype=np.float32) #rescale
+            gt_boxes_cp = gt_boxes.copy()
+            gt_boxes_cp *= image_array
             box = np.asarray(box, dtype=np.float32).reshape(5,)
-            if box[-1] > 0.7: # Skip if confidence is high
-                continue
+            # if box[-1] < 0.9: # Skip if confidence is high
+            #     continue
 
             env = BoxRefinementEnv(
                 image=img,
-                gt_boxes=gt_boxes,
+                gt_boxes=gt_boxes_cp,
                 initial_box=box,
                 feature_extractor=feature_extractor,
                 iou_fn=compute_iou